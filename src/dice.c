0bcf83e9683e3b731cf3c89a7e36341914912a5a
oceaquaris <oceaquarisoxyheloris@gmail.com>
2017-07-19 19:08:57 -0400

    Dice rolling functionality added.

---
+++ src/dice.c
@@ -0,0 +1,201 @@
+/*
+ * Code ruthlessly stolen from Haxima's nazgul engine
+ *
+ * dice.c
+ *
+ *    Created on: 10 July 2017
+ *        Author: oceaquaris
+ * Last Modified:
+ *
+ * Field Overview:
+ *  static:
+ *      dice_parse
+ *  extern:
+ *      dice_average
+ *      dice_roll
+ *      dice_roll_numeric
+ *      dice_valid
+ */
+
+#include <assert.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#include "dice.h"
+
+
+
+static int dice_parse(const char *ptr, int *num, int *faces, int *bias) {
+    int state = 0,
+        val = 0,
+        sign = 1;
+
+    *num = 0;
+    *faces = 0;
+    *bias = 0;
+
+    while (*ptr) {
+        switch (state) {
+            case 0:
+                if (*ptr == '+') {
+                    sign = 1;
+                    state = 4;
+                }
+                else if (*ptr == '-') {
+                    sign = -1;
+                    state = 4;
+                }
+                else if (isdigit(*ptr)) {
+                    val = *ptr - '0';
+                    if (val)
+                        state = 1;
+                }
+                else {
+                    goto error;
+                }
+                break;
+            case 1:
+                if (*ptr == 'd') {
+                    *num = val * sign;
+                    val = 0;
+                    sign = 1;
+                    state = 2;
+                }
+                else if (isdigit(*ptr)) {
+                    val = (val * 10) + *ptr - '0';
+                }
+                else {
+                    goto error;
+                }
+                break;
+            case 2:
+                if (isdigit(*ptr)) {
+                    val = *ptr - '0';
+                    if (val)
+                        state = 3;
+                }
+                else {
+                    goto error;
+                }
+                break;
+            case 3:
+                if (isdigit(*ptr)) {
+                    val = (val * 10) + *ptr - '0';
+                }
+                else if (*ptr == '+') {
+                    *faces = val;
+                    val = 0;
+                    state = 4;
+                    sign = 1;
+                }
+                else if (*ptr == '-') {
+                    *faces = val;
+                    val = 0;
+                    state = 4;
+                    sign = -1;
+                }
+                else {
+                    goto error;
+                }
+                break;
+
+            case 4:
+                if (isdigit(*ptr)) {
+                    val = atoi(ptr);
+                    if (val)
+                        state = 5;
+                }
+                else {
+                    goto error;
+                }
+                break;
+
+            case 5:
+                if (isdigit(*ptr)) {
+                    val = (val * 10) + *ptr - '0';
+                }
+                else {
+                    goto error;
+                }
+                break;
+            default:
+                assert(false); /* impossible state */
+                goto error;
+        }
+
+        ptr++;
+    }
+
+    switch (state) {
+        case 0:
+            break;
+        case 1:
+            *bias = val * sign;
+            break;
+        case 2:
+            goto error;
+            break;
+        case 3:
+            *faces = val * sign;
+            break;
+        case 4:
+            *bias = val * sign;
+            break;
+        case 5:
+            *bias = val * sign;
+            break;
+        default:
+            assert(false); /* impossible state */
+            goto error;
+    }
+
+    return 0;
+
+error:
+    return -1;
+}
+
+
+
+int dice_average(const char *fmt) {
+    int num, faces, bias;
+    dice_parse(fmt, &num, &faces, &bias);
+    return ((faces / 2) + 1) * num + bias;
+}
+
+
+
+int dice_roll(const char *ptr) {
+    int num = 0,
+        faces = 0,
+        bias = 0;
+
+    if (dice_parse(ptr, &num, &faces, &bias)) {
+        assert(false); /* if uncertain, caller should have checked
+                        * first */
+        return 0;
+    }
+
+    return dice_roll_numeric(num,faces,bias);
+}
+
+
+
+int dice_roll_numeric(int num, int faces, int bias) {
+    int val = 0;
+    while (num--) {
+        val += (rand() % faces) + 1;
+    }
+    val += bias;
+    return val;
+}
+
+
+
+int dice_valid(const char *fmt) {
+    int num, faces, bias;
+    if (!fmt)
+        return 0;
+    return dice_parse(fmt, &num, &faces, &bias) ? 0 : 1;
+}
---
+++ src/dice.h
@@ -0,0 +1,28 @@
+#ifndef DICE_H
+#define DICE_H
+
+/* A dice roll is specified by a string format. For example, "2d20+6". The
+ * legal formats are defined by a regular expression:
+ *
+ *      ([1-9][0-9]*]d[1-9][0-9]*)?([\+\-]?[1-9][0-9]*)?
+ *
+ * Which means the following are all legal examples:
+ *
+ *      "1d1"
+ *      "2"
+ *      "" (note: returns 0)
+ *      "5064d21023902-10909012"
+ *
+ * The result of the dice roll is returned. If the format is invalid then it
+ * will always return 0, but since 0 is a valid response you won't necessarily
+ * be able to tell. To check if a format is bad use the separate dice_valid()
+ * call, which returns non-zero if the format is ok and 0 otherwise.
+ *
+ */
+
+extern int dice_average      (const char *fmt);
+extern int dice_roll         (const char *fmt);
+extern int dice_roll_numeric (int num, int faces, int bias);
+extern int dice_valid        (const char *fmt);
+
+#endif
